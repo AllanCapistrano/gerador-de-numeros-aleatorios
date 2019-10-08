2d0b134457ecd7d478a4cd1391d3207a87cf416e
Allan Capistrano <43974590+AllanCapistrano@users.noreply.github.com>
2019-10-08 13:27:27 -0300

    Update gerador-de-numeros-aleatorios.c

--- gerador-de-numeros-aleatorios.c
+++ gerador-de-numeros-aleatorios.c
@@ -9,40 +9,44 @@
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-	int ext1, qtd, aux, status, i, j, k, l, m;
+	int qtd, aux, status, i, j, k, l, m;
 	
 	printf("Digite a quantidade de números aleatórios você deseja: ");
 	scanf("%d", &qtd);
 	printf("\n");
 	
-	int V[qtd + 1];
+	if(qtd > LIMIT)
+		printf("Não é possível gerar essa quantidade de números, pois excede o valor máximo possível. Altere o número máximo modificando a constante \"LIMIT\" ou digite outra quanitdade de números aleatórios e tente novamente!\n\n");
+	else{
+		int V[qtd + 1];
 	
-	srand(time(NULL));
-	
-	for(i=0; i<qtd; i++){
-		do{
-			V[i] = 1 + (rand() % LIMIT);
-			status = VALID;
-			for(m=0; m<i; m++){
-				if(V[i] == V[m])
-					status = INVALID;
-			}
-		}while(status == INVALID);
-	}
-	
-	/*Laço para ordenação dos números*/
-	for(j=1; j<qtd; j++){
-		aux = V[j];
-		for(k=j; (k>0) && (aux < V[k-1]); k--){
-			V[k] = V[k-1];
+		srand(time(NULL));
+		
+		for(i=0; i<qtd; i++){
+			do{
+				V[i] = 1 + (rand() % LIMIT);
+				status = VALID;
+				for(m=0; m<i; m++){
+					if(V[i] == V[m])
+						status = INVALID;
+				}
+			}while(status == INVALID);
 		}
-		V[k] = aux;
+		
+		/*Laço para ordenação dos números*/
+		for(j=1; j<qtd; j++){
+			aux = V[j];
+			for(k=j; (k>0) && (aux < V[k-1]); k--){
+				V[k] = V[k-1];
+			}
+			V[k] = aux;
+		}
+		
+		for(l=0; l<qtd; l++)
+			printf("%d ", V[l]);
+		
+		printf("\n\n");
 	}
-	
-	for(l=0; l<qtd; l++)
-		printf("%d ", V[l]);
-	
-	printf("\n\n");
 	system("pause");
 	return 0;
 }
