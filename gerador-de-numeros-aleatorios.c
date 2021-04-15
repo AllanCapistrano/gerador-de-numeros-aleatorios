06167b70b551720038c80db0d8aa1288e356fa01
Allan Capistrano <43974590+AllanCapistrano@users.noreply.github.com>
2021-04-14 22:33:52 -0300

    Voltando código antigo.

--- gerador-de-numeros-aleatorios.c
+++ gerador-de-numeros-aleatorios.c
@@ -3,8 +3,11 @@
 #include<time.h>
 #include <locale.h>
 
+#define VALID 1
+#define INVALID 0
+
 int main(){
-	int limit;
+    	int limit;
 	setlocale(LC_ALL, "Portuguese");
 	printf("Digite o número limite que pode ser gerado: ");
 	scanf("%d", &limit);
@@ -15,26 +18,38 @@ int main(){
 	scanf("%d", &qtd);
 	printf("\n");
 	
-	int V[qtd + 1];
+	if(qtd > limit)
+		printf("Não é possível gerar essa quantidade de números, pois excede o valor máximo possível. Altere o número máximo modificando a constante \"LIMIT\" ou digite outra quanitdade de números aleatórios e tente novamente!\n\n");
+	else{
+		int V[qtd + 1];
 	
-	srand(time(NULL));
-	
-	for(i=0; i<qtd; i++)
-		V[i] = 1 + (rand() % limit);
-	
-	/*Laço para ordenação dos números*/
-	for(j=1; j<qtd; j++){
-		aux = V[j];
-		for(k=j; (k>0) && (aux < V[k-1]); k--){
-			V[k] = V[k-1];
+		srand(time(NULL));
+		
+		for(i=0; i<qtd; i++){
+			do{
+				V[i] = 1 + (rand() % limit);
+				status = VALID;
+				for(m=0; m<i; m++){
+					if(V[i] == V[m])
+						status = INVALID;
+				}
+			}while(status == INVALID);
 		}
-		V[k] = aux;
+		
+		/*Laço para ordenação dos números*/
+		for(j=1; j<qtd; j++){
+			aux = V[j];
+			for(k=j; (k>0) && (aux < V[k-1]); k--){
+				V[k] = V[k-1];
+			}
+			V[k] = aux;
+		}
+		
+		for(l=0; l<qtd; l++)
+			printf("%d ", V[l]);
+		
+		printf("\n\n");
 	}
-	
-	for(l=0; l<qtd; l++)
-		printf("%d ", V[l]);
-	
-	printf("\n\n");
 	system("pause");
 	return 0;
 }
